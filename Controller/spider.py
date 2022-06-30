a62821bbba885e7e67b53eea55ce544c23440131
zengqiqi <qiqi.zeng@ebestmobile.com>
2022-06-30 19:05:32 +0800

    spider

--- Controller/spider.py
+++ Controller/spider.py
@@ -1,6 +1,45 @@
 import urllib3
 import requests
 import re
+import os
+from queue import Queue
+import time
+
+
+#logger = logging.getLogger('flask.app')
+#logger.info('flask.app')
+#from flask import current_app
+#current_app.logger.info('logged by current_app from main')
+
+q=Queue();
+errlist=[]
+suclist=[]
+isstart=False;
+def AddUrl(url):
+    q.put(url)
+    return "ok";
+
+def Start():
+    isstart=True
+    exec();
+
+def Stop():
+    isstart=False
+
+def exec():
+    while isstart:
+        if not q.empty():
+            url=q.get()
+            try:
+                html=gethtml(url)
+                getphoto(html)
+                suclist.append(url)
+                #current_app.logger.info('下载：'+url+' 成功')
+            except Exception as e:
+                #current_app.logger.info('下载：'+url+'报错：'+ e)
+                errlist.append(url)
+
+        time.sleep(3);
 
 def gethtml(url): 
     #  忽略警告：InsecureRequestWarning: Unverified HTTPS request is being made. Adding certificate verification is strongly advised.
@@ -25,7 +64,11 @@ def getphoto(html):
     http = urllib3.PoolManager();
     header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36'} 
     print(result)
-    print(html);
+    folder='F:/图片/test/'+title;
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    
+    #print(html);
     for res in result:
         r = http.request('GET',
              res,
@@ -33,17 +76,17 @@ def getphoto(html):
              headers=header)
         filename=res.split("/")[-1]
         print(r.status) # 200
-        with open('F:/图片/test/'+filename,'wb') as f:
+        with open(folder+'/'+filename,'wb') as f:
             f.write(r.data);
 
 #reg="ess-data[\w\W]*?http.*?(jpg|gif|png|bmp)"
 #reg="ess-data</span>='<[^>]+>(.*?)</[^>]+>"
 reg="<img.*?ess-data='(.*?)'>"
 regtitle="<title>(.*?)</title>"
-html=gethtml("");
+#html=gethtml("");
 # with open("D:/pcode/growpupdaily_api/Asset/test.html") as file_object:
 #     contends = file_object.read();
 #     getphoto(contends);
-getphoto(html);
+#getphoto(html);
     
 
--- index.py
+++ index.py
@@ -4,9 +4,12 @@ from flask import Flask, jsonify
 from flask import request
 from flask_cors import CORS
 from jinja2 import Undefined
+import threading
+import logging
 
 from Comm import JsonEncoderCustom
 from Controller import Photo
+from Controller import spider
 import datetime
 
 # 实例化，可视为固定格式
@@ -70,6 +73,26 @@ def getPhotoById():
     post_param = request.json
     return Success(Photo.Photo().getPhotoById(post_param.get('userid'),post_param.get('photoid')))
 
+@app.route('/spider/start',methods=['Get'])
+def spiderStart():
+    thread = threading.Thread(target=spider.Start)
+    thread.start()
+    return Success("spider start ok")
+
+@app.route('/spider/stop',methods=['Get'])
+def spiderStop():
+    spider.Stop()
+    return Success("spider start ok")
+
+@app.route('/spider/qsize',methods=['Get'])
+def spiderQsize():
+    return Success({"qsize": spider.q.qsize(),"errlist":spider.errlist,"suclist":spider.suclist})
+
+@app.route('/spider/addurl',methods=['Get'])
+def spiderAddurl():
+    url=request.args.get("url")
+    return Success(spider.AddUrl(url))
+
 def Success(data,message=None):
     return JsonEncoderCustom.JsonEncoderCustom().encode({
         'code':0,
@@ -86,7 +109,20 @@ def Fail(message=None,code=1):
         'operationTime':datetime.datetime.now()
     })
 
+def SetLogger():
+    # 设置日志的记录等级
+    logging.basicConfig(level=logging.DEBUG) # 调试debug级
+    # 创建日志记录器，指明日志保存的路径、每个日志文件的最大大小、保存的日志文件个数上限
+    file_log_handler =logging.handlers.RotatingFileHandler("logs/log", maxBytes=1024*1024*100, backupCount=10)
+    # 创建日志记录的格式 日志等级 输入日志信息的文件名 行数 日志信息
+    formatter = logging.Formatter('%(levelname)s %(filename)s:%(lineno)d %(message)s')
+    # 为刚创建的日志记录器设置日志记录格式
+    file_log_handler.setFormatter(formatter)
+    # 为全局的日志工具对象（flask app使用的）添加日志记录器
+    logging.getLogger().addHandler(file_log_handler)
+
 if __name__ == '__main__':
     # app.run(host, port, debug, options)
     # 默认值：host="127.0.0.1", port=5000, debug=False
+    #SetLogger()
     app.run(host="0.0.0.0", port=5000)
